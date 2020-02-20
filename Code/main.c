09f310dda1b2317e71cf6190befc199e19e9a136
Lingyu Zhang <171240524@smail.nju.edu.cn>
2020-02-20 23:24:50 +0800

    write main.c

---
+++ Code/main.c
@@ -0,0 +1,12 @@
+
+int main(int argc, char** argv){
+    if(argc > 1){
+        if(!(yyin = fopen(argv[1], "r"))){
+            perror(argv[1]);
+            return 1;
+
+        }
+    }
+    while(yylex() != 0);
+    return 0;
+}
